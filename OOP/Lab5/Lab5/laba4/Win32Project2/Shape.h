b3007247d95307bd994702e585beba9c5cc9ac35
Slavick Vinnitskyi <andrey.belyi1@gmail.com>
2018-07-17 17:32:28 +0300

    Add files via upload

---
+++ OOP/Lab5/Lab5/laba4/Win32Project2/Shape.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+class Shape
+{
+protected:
+	long X1 = 0, Y1 = 0, X2 = 0, Y2 = 0;
+public:
+	virtual void Draw(HDC hdc) = 0;
+	void SetX1Y1(long x1, long y1)
+	{
+		X1 = x1;
+		Y1 = y1;
+	}
+	void SetX2Y2(long x2, long y2)
+	{
+		X2 = x2;
+		Y2 = y2;
+	}
+	long x1()
+	{
+		return X1;
+	}
+	long x2()
+	{
+		return X2;
+	}
+	long y1()
+	{
+		return Y1;
+	}
+	long y2()
+	{
+		return Y2;
+	}
+};
