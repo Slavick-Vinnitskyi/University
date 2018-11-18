361f6700438903e2b66511dfd7c67c6915091a78
Slavick Vinnitskyi <andrey.belyi1@gmail.com>
2018-11-18 19:10:28 +0200

    Add Laba 6
    Simple multithreading implementation in MPI

---
+++ Multithreading/Laba6final/Laba6final/Function3.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "Function3.h"
+
+Function3::Function3(int sizeOfArrays)
+{
+	this->sizeOfArrays = sizeOfArrays;
+}
+
+
+Function3::~Function3()
+{
+}
+
+
+void Function3::StartFunction(unsigned int& start) {
+
+	unsigned int myStart = clock();
+
+	Methods methods(sizeOfArrays);
+	T = methods.fillVector();
+
+	MP = methods.fillMatrix();
+	MR = methods.fillMatrix();
+
+	O = methods.MultMatrixVector(methods.transMatrix(methods.multMatrix(MP, MR)), T);
+
+	unsigned int myEnd = clock();
+	start = myEnd - myStart;
+
+#pragma omp critical
+	{
+		printf("Function 3 started\n");
+		methods.putVector(O);
+		printf("Function 3 ended\n");
+	}
+}
